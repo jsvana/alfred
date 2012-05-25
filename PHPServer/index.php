2838fb88497cff429d67fcd3622f439aaac38e33
Jay Vana <jsvana@mtu.edu>
2012-05-25 17:39:46 -0400

    Implementing commands.

--- PHPServer/index.php
+++ PHPServer/index.php
@@ -1,4 +1,6 @@
 <?php
+	session_start();
+
 	$data = json_decode($_POST['json']);
 	
 	$method = $data->{'method'};
@@ -14,9 +16,30 @@
 			$password = $data->{'params'}->{'password'};
 
 			if(mysql_num_rows(mysql_query("SELECT `username` FROM `users` WHERE `username`='" . mysql_real_escape_string($username) . "' AND `password`='" . md5($password) . "';")) > 0) {
-				$ret = "{\"result\":{\"key\":\"" . md5($username . $password . date()) . "\"}}";
+				$key = md5($username . $password . date());
+				$_SESSION['key'] = $key;
+
+				$ret = "{\"result\":{\"key\":\"" . $key . "\"}}";
 			} else {
 				$ret = "{\"error\":{\"code\":-2,\"message\":\"Incorrect username or password.\",\"data\":{}}}";
+			}
+			break;
+		case "Password.Retrieve":
+			if($data->{'key'} !== $_SESSION['key']) {
+				$ret = "{\"error\":{\"code\":-3,\"message\":\"Not authenticated.\",\"data\":{}}}";
+			} else if(!isset($data->{'params'}->{'master'})) {
+				$ret = "{\"error\":{\"code\":-4,\"message\":\"Invalid parameters.\",\"data\":{\"message\":\"Parameter 'master' not set.\"}}}";
+			} else if(!isset($data->{'params'}->{'site'})) {
+				$ret = "{\"error\":{\"code\":-4,\"message\":\"Invalid parameters.\",\"data\":{\"message\":\"Parameter 'site' not set.\"}}}";
+			} else {
+				$result = mysql_query("SELECT `password` FROM `passwords` WHERE `site`='" . mysql_real_escape_string($data->{'params'}->{'site'} . "';"));
+				if(mysql_num_rows($result) === 0) {
+					$ret = "{\"error\":{\"code\":-5,\"message\":\"Site not in database.\",\"data\":{}}}";
+				} else {
+					$row = mysql_fetch_assoc($result);
+					$pass = decrypt($row['password'], $data->{'params'}->{'master'});
+					$ret = "{\"result\":{\"password\":\"" . $pass . "\"}}";
+				}
 			}
 			break;
 		default:
@@ -25,4 +48,8 @@
 	}
 
 	echo $ret;
+
+	function decrypt($crypt, $key) {
+		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $key, base64_decode($crypt), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
+	}
 ?>
