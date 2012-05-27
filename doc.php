b49919b8cda7cc9410c70f1ff6a31ba42db37e0e
Jay Vana <jsvana@mtu.edu>
2012-05-27 14:46:01 -0400

    I gotta stop doing this.

--- doc.php
+++ doc.php
@@ -9,6 +9,16 @@
 		foreach($namespace->methods as $method) {
 			fwrite($fp, "**" . $method->method . "**\n\n");
 			fwrite($fp, $method->description . "\n\n");
+
+			if(count($method->parameters) == 0) {
+				fwrite($fp, "*Parameters:* `none`\n\n");
+			} else {
+				fwrite($fp, "*Parameters:*\n");
+
+				foreach($method->parameters as $parameter) {
+					fwrite($fp, "`" . $parameter->name . " (" . $parameter->type . ")`, " . $parameter->description . "  \n");
+				}
+			}
 		}
 	}
 
