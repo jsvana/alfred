bf30404fea1340b89ca9da0a8b0c5e1fca7ab308
Kaleb Elwert <kelwert@mtu.edu>
2012-07-03 10:07:43 -0400

    gofmt

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"unicode/utf8"
-	"strings"
-	"regexp"
 	"encoding/json"
+	"fmt"
+	"regexp"
+	"strings"
+	"unicode/utf8"
 )
 
 type Result []Token
@@ -17,16 +17,16 @@ const (
 	AQL_ARG
 	AQL_OP
 	AQL_FUNC
-	VAR_DELIMITER = "`"
+	VAR_DELIMITER   = "`"
 	FUNCTION_PARENS = "()"
 )
 
 // The extra `json:` crap is to make sure it gets lower cased
 type Response struct {
-	Alfred string				`json:"alfred"`
-	Key string					`json:"key"`
-	Method string				`json:"method"`
-	Params map[string]string	`json:"params"`
+	Alfred string            `json:"alfred"`
+	Key    string            `json:"key"`
+	Method string            `json:"method"`
+	Params map[string]string `json:"params"`
 }
 
 type Token struct {
@@ -35,14 +35,14 @@ type Token struct {
 }
 
 type Param struct {
-	Key string
+	Key   string
 	Value string
 }
 
 type Query struct {
-	Table string
+	Table  string
 	Fields []string
-	Where map[string]string
+	Where  map[string]string
 }
 
 func lg(str string) {
@@ -52,7 +52,7 @@ func lg(str string) {
 func nextChar(str string) (string, string) {
 	var ret string
 	first, _ := utf8.DecodeRune([]byte(str))
-	if (len(str) > 1) {
+	if len(str) > 1 {
 		ret = str[1:]
 	} else {
 		ret = ""
@@ -60,7 +60,7 @@ func nextChar(str string) (string, string) {
 	return string(first), ret
 }
 
-func peekChar(str string) (string) {
+func peekChar(str string) string {
 	first, _ := utf8.DecodeRune([]byte(str))
 	return string(first)
 }
@@ -68,7 +68,7 @@ func peekChar(str string) (string) {
 func isValidChar(str string) (ret bool) {
 	reg, err := regexp.Compile("[0-9a-zA-Z_]")
 	ret = false
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error in regexp.")
 	} else {
 		ret = reg.Match([]byte(str))
@@ -80,7 +80,7 @@ func isValidChar(str string) (ret bool) {
 func isOper(str string) (ret bool) {
 	reg, err := regexp.Compile("[=\\.,;]")
 	ret = false
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error in regexp.")
 	} else {
 		ret = reg.Match([]byte(str))
@@ -92,7 +92,7 @@ func isOper(str string) (ret bool) {
 func isSpace(str string) (ret bool) {
 	reg, err := regexp.Compile("\\s")
 	ret = false
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error in regexp.")
 	} else {
 		ret = reg.Match([]byte(str))
@@ -101,9 +101,9 @@ func isSpace(str string) (ret bool) {
 	return
 }
 
-func contains(arr []string, val string) (bool) {
+func contains(arr []string, val string) bool {
 	for _, a := range arr {
-		if (a == val) {
+		if a == val {
 			return true
 		}
 	}
@@ -113,7 +113,7 @@ func contains(arr []string, val string) (bool) {
 func niceString(str string) (ret string) {
 	ret = str
 	reg, err := regexp.Compile("[\n\t]")
-	if (err != nil) {
+	if err != nil {
 		fmt.Println("Error in regexp.")
 	} else {
 		ret = string(reg.ReplaceAll([]byte(str), []byte("")))
@@ -126,21 +126,21 @@ func (r *Result) addToken(tokText string, tokType int) {
 	*r = append(*r, Token{Type: tokType, Text: tokText})
 }
 
-func (r *Result) nextToken() (Token) {
+func (r *Result) nextToken() Token {
 	ret := (*r)[0]
 	*r = (*r)[1:]
 	return ret
 }
 
-func (r *Result) peekToken() (Token) {
+func (r *Result) peekToken() Token {
 	return (*r)[0]
 }
 
-func (t *Result) hasNextToken() (bool) {
+func (t *Result) hasNextToken() bool {
 	return len(*t) != 0
 }
 
-func (t *Token) tokenTextEq(text string) (bool) {
+func (t *Token) tokenTextEq(text string) bool {
 	return t.Text == text || strings.ToLower(t.Text) == text
 }
 
@@ -149,41 +149,41 @@ func parseAQL(command string, apiKey string) (string, []string) {
 
 	keywords := []string{"select", "insert", "delete", "from", "where", "and", "or"}
 
-	var methods = map[string] string {
-		"weather": "Location.Weather",
-        "directions": "Location.Directions",
-        "iplookup": "Location.IPLookup",
-        "minecraft_motd": "Minecraft.MOTD",
-        "minecraft_players": "Minecraft.Players",
-        "minecraft_maxplayers": "Minecraft.MaxPlayers",
-        "bitbucket_status": "Net.Bitbucket.Status",
-        "github_status": "Net.Github.Status",
-        "heroku_status": "Net.Heroku.Status",
-        "movie": "Net.TMDB.Movie",
-        "ping": "Net.Ping",
-        "dns": "Net.DNS",
-        "shorten": "Net.Shorten",
-        "lmgtfy": "Net.LMGTFY",
-        "tasks": "Tasks.List",
+	var methods = map[string]string{
+		"weather":              "Location.Weather",
+		"directions":           "Location.Directions",
+		"iplookup":             "Location.IPLookup",
+		"minecraft_motd":       "Minecraft.MOTD",
+		"minecraft_players":    "Minecraft.Players",
+		"minecraft_maxplayers": "Minecraft.MaxPlayers",
+		"bitbucket_status":     "Net.Bitbucket.Status",
+		"github_status":        "Net.Github.Status",
+		"heroku_status":        "Net.Heroku.Status",
+		"movie":                "Net.TMDB.Movie",
+		"ping":                 "Net.Ping",
+		"dns":                  "Net.DNS",
+		"shorten":              "Net.Shorten",
+		"lmgtfy":               "Net.LMGTFY",
+		"tasks":                "Tasks.List",
 	}
 
 	var tokens Result
 	var query Query
 	query.Where = make(map[string]string)
 
-	for (len(input) > 0) {
+	for len(input) > 0 {
 		tokText := ""
 		tokType := AQL_STR
 
-		if (isSpace(peekChar(input))) {
+		if isSpace(peekChar(input)) {
 			_, input = nextChar(input)
-		} else if (isOper(peekChar(input))) {
+		} else if isOper(peekChar(input)) {
 			tokText, input = nextChar(input)
 			tokens.addToken(tokText, AQL_OP)
-		} else if (peekChar(input) == VAR_DELIMITER) {
+		} else if peekChar(input) == VAR_DELIMITER {
 			_, input = nextChar(input)
 
-			for (peekChar(input) != VAR_DELIMITER && len(input) > 0) {
+			for peekChar(input) != VAR_DELIMITER && len(input) > 0 {
 				var n string
 				n, input = nextChar(input)
 				tokText += n
@@ -191,10 +191,10 @@ func parseAQL(command string, apiKey string) (string, []string) {
 
 			_, input = nextChar(input)
 			tokens.addToken(tokText, AQL_FIELD)
-		} else if (peekChar(input) == "'") {
+		} else if peekChar(input) == "'" {
 			_, input = nextChar(input)
 
-			for (peekChar(input) != "'" && len(input) > 0) {
+			for peekChar(input) != "'" && len(input) > 0 {
 				var n string
 				n, input = nextChar(input)
 				tokText += n
@@ -203,12 +203,12 @@ func parseAQL(command string, apiKey string) (string, []string) {
 			_, input = nextChar(input)
 			tokens.addToken(tokText, AQL_ARG)
 		} else {
-			for (len(input) > 0 && isValidChar(peekChar(input))) {
+			for len(input) > 0 && isValidChar(peekChar(input)) {
 				var n string
 				n, input = nextChar(input)
 				tokText += n
 
-				if (input[0:2] == "()") {
+				if input[0:2] == "()" {
 					tokType = AQL_FUNC
 					_, input = nextChar(input)
 					_, input = nextChar(input)
@@ -216,7 +216,7 @@ func parseAQL(command string, apiKey string) (string, []string) {
 				}
 			}
 
-			if (contains(keywords, tokText) || contains(keywords, strings.ToLower(tokText))) {
+			if contains(keywords, tokText) || contains(keywords, strings.ToLower(tokText)) {
 				tokens.addToken(tokText, AQL_KEYWD)
 			} else {
 				tokens.addToken(tokText, tokType)
@@ -224,47 +224,47 @@ func parseAQL(command string, apiKey string) (string, []string) {
 		}
 	}
 
-	for (tokens.hasNextToken()) {
+	for tokens.hasNextToken() {
 		token := tokens.nextToken()
 
-		if (token.Type == AQL_KEYWD && token.tokenTextEq("select")) {
+		if token.Type == AQL_KEYWD && token.tokenTextEq("select") {
 			for tokens.hasNextToken() {
 				var nToken Token
 				nToken = tokens.nextToken()
 				query.Fields = append(query.Fields, nToken.Text)
-				if (tokens.peekToken().Type != AQL_KEYWD) {
+				if tokens.peekToken().Type != AQL_KEYWD {
 					tokens.nextToken()
 				} else {
 					break
 				}
 			}
-		} else if (token.Type == AQL_KEYWD && token.tokenTextEq("from")) {
+		} else if token.Type == AQL_KEYWD && token.tokenTextEq("from") {
 			var nToken Token
 			nToken = tokens.nextToken()
 			query.Table = nToken.Text
-		} else if (token.Type == AQL_KEYWD && token.tokenTextEq("where")) {
+		} else if token.Type == AQL_KEYWD && token.tokenTextEq("where") {
 			for tokens.hasNextToken() {
 				peek := tokens.peekToken()
 
-				if (peek.Type == AQL_OP && peek.Text == ";") {
+				if peek.Type == AQL_OP && peek.Text == ";" {
 					tokens.nextToken()
 					break
-				} else if (len(tokens) > 5) {
+				} else if len(tokens) > 5 {
 					var param Token
 					var next Token
 					param = tokens.nextToken()
 					tokens.nextToken()
 					next = tokens.nextToken()
 
-					if (next.Type == AQL_FUNC && next.Text == "me") {
+					if next.Type == AQL_FUNC && next.Text == "me" {
 						tokens.nextToken()
 						tokens.nextToken()
 
 						peek = tokens.peekToken()
 
-						if (peek.Type == AQL_KEYWD && peek.Text == "and") {
+						if peek.Type == AQL_KEYWD && peek.Text == "and" {
 							tokens.nextToken()
-						} else if (peek.Type == AQL_OP && peek.Text == ";") {
+						} else if peek.Type == AQL_OP && peek.Text == ";" {
 							tokens.nextToken()
 							break
 						}
@@ -273,13 +273,13 @@ func parseAQL(command string, apiKey string) (string, []string) {
 
 						peek = tokens.peekToken()
 
-						if (peek.Type == AQL_KEYWD && peek.tokenTextEq("and")) {
+						if peek.Type == AQL_KEYWD && peek.tokenTextEq("and") {
 							tokens.nextToken()
-						} else if (peek.Type == AQL_OP && peek.Text == ";") {
+						} else if peek.Type == AQL_OP && peek.Text == ";" {
 							tokens.nextToken()
 						}
 					}
-				} else if (len(tokens) > 3) {
+				} else if len(tokens) > 3 {
 					var param Token
 					var next Token
 
@@ -289,9 +289,9 @@ func parseAQL(command string, apiKey string) (string, []string) {
 
 					query.Where[param.Text] = next.Text
 
-					if (peek.Type == AQL_KEYWD && peek.tokenTextEq("and")) {
+					if peek.Type == AQL_KEYWD && peek.tokenTextEq("and") {
 						tokens.nextToken()
-					} else if (peek.Type == AQL_OP && peek.Text == ";") {
+					} else if peek.Type == AQL_OP && peek.Text == ";" {
 						tokens.nextToken()
 						break
 					}
@@ -313,7 +313,7 @@ func main() {
 	out, returns := parseAQL(command, "")
 	fmt.Println("out:", out)
 	fmt.Println("returns:")
-	for _, f := range(returns) {
+	for _, f := range returns {
 		fmt.Println(f)
 	}
 }
